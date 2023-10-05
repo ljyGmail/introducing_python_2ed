17e12deb96e4f5b27b69703c685a99b70b9b9fa0
liangjinyong <liangjy881@gmail.com>
2023-10-06 00:44:56 +0900

    ch08 dictionaries and sets: finished

---
+++ ch08_dictionaries_and_sets/03_data_structures_so_far.py
@@ -0,0 +1,15 @@
+# @ Data Structures So Far
+
+marx_list = ['Grouch', 'Chico', 'Harpo']
+marx_tuple = ('Groucho', 'Chico', 'Harpo')
+marx_dict = {'Groucho': 'banjo', 'Chico': 'piano', 'Harpo': 'harp'}
+marx_set = {'Groucho', 'Chico', 'Harpo'}
+
+print(marx_list[2])
+print(marx_tuple[2])
+print(marx_dict['Harpo'])
+
+print('Harpo' in marx_list)
+print('Harpo' in marx_tuple)
+print('Harpo' in marx_dict)
+print('Harpo' in marx_set)
