41244b4ff75401446bfc8e4c1a1a4b1db97a52e8
liangjinyong <liangjy881@gmail.com>
2023-10-06 11:50:07 +0900

    combine and  duplicate strings

---
+++ ch05_text_strings/04_combine_by_using_plus.py
@@ -0,0 +1,18 @@
+# @ Combine by Using +
+
+print('Release the kraken! ' + 'No, wait!')
+
+# combine literal strings just by having one after the other
+print('My word! ' 'A gentleman caller!')
+
+vowels = ('a'
+          "e" '''i'''
+          'o' """u""")
+print(vowels)
+
+a = 'Duck.'
+b = a
+c = 'Grey Duck!'
+print(a + b + c)
+
+print(a, b, c)
